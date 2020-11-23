bf041d8e37d89f62796c6825de6ec5a4b055a266
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-11-24 01:41:07 +0330

    More accurate test

--- test/test.rb
+++ test/test.rb
@@ -7,10 +7,11 @@ pages_min = 1
 while pages_min < pages_max do 
     data = Tomato::GettyImages.new("pizza", pages_min) 
     links = links + data.get_links 
+    puts "Links for iteration #{pages_min} appended. "
     pages_min += 1 
 end 
 
-f = File.open('links.txt') 
+f = File.open('links.txt', 'w') 
 
 links.each do |link|
     link = link + "\n" 

4fffcadf4f32e6f6fd25c56c581d7fcdfdb09823
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-11-23 17:41:41 +0330

    GettyImages for later

--- lib/tomato_downloader.rb
+++ lib/tomato_downloader.rb
@@ -21,30 +21,4 @@ module Tomato
             return body
         end 
     end 
-
-    class GettyImages
-        def initialize(keyword)
-            @keyword = keyword
-        end
-
-        def get_links()
-            web_page = Nokogiri::HTML(open("https://www.gettyimages.co.uk/photos/#{@keyword}")) 
-            events_holder = web_page.xpath('//*[@id="gallery"]')
-
-            links = [] 
-            clean_links = []
-            events_holder.each do |event| 
-                link = event.css("a") 
-                links << event 
-            end 
-
-            #links = links[0] 
-            
-            #links.each do |link| 
-            #   clean_links << link['href'] 
-            #end 
-
-            return links
-        end
-    end
 end

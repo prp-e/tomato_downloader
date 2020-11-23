fa2c030c5db1c38b034f6f72908be92aba15d7b6
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-11-23 14:14:04 +0330

    wrote the main download method

--- lib/tomato_downloader.rb
+++ lib/tomato_downloader.rb
@@ -4,4 +4,9 @@ class TomatoDownloader
     def initialize(image_link) 
         @image_link = image_link 
     end 
+
+    def get_image()
+        request = HTTParty.get("http://tomato.to/toma.php", :query => {'url' => @image_link})
+        return request.body 
+    end 
 end

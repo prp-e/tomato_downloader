b283b5f6a471865b8b99e3eeeea23632fda81f7a
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-11-24 02:35:55 +0330

    Working on download link generator

--- test/test_download.rb
+++ test/test_download.rb
@@ -7,3 +7,18 @@ links = []
 f.readlines.each do |line| 
     links << line 
 end 
+
+download_links = [] 
+
+links.each do |link| 
+    document = Tomato::TomatoDownloader(link) 
+    document = document.get_image 
+    download_links << document 
+end  
+
+f = File.open('download.txt', 'w') 
+
+download_links.each do |dl| 
+    dl = dl + "\n" 
+    f.write(dln) 
+end 

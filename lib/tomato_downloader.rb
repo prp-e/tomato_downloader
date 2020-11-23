1596cf2f195248f765fe9131a7097ec5dd63ac9f
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-11-23 16:42:06 +0330

    Writing gettyimages crawler

--- lib/tomato_downloader.rb
+++ lib/tomato_downloader.rb
@@ -2,18 +2,24 @@ require 'httparty'
 require 'json'
 require 'base64'
 
-class TomatoDownloader
-    def initialize(image_link) 
-        @image_link = image_link 
+module Tomato
+
+    class TomatoDownloader
+        def initialize(image_link) 
+            @image_link = image_link 
+        end 
+
+        def get_image()
+            request = HTTParty.get("http://tomato.to/toma.php", :query => {'url' => @image_link})
+            body = JSON.parse(request.body) 
+            body = body['data']
+            body = body[34..-1] 
+            body = Base64.decode64(body) 
+
+            return body
+        end 
     end 
 
-    def get_image()
-        request = HTTParty.get("http://tomato.to/toma.php", :query => {'url' => @image_link})
-        body = JSON.parse(request.body) 
-        body = body['data']
-        body = body[34..-1] 
-        body = Base64.decode64(body) 
-
-        return body
-    end 
+    class GettyImages
+    end
 end
